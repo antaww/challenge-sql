85c3f01abce8e9238b3c561de49b7aeffcd3c9d4
antaww <anta.pizzetta@gmail.com>
2022-12-26 19:03:30 +0100

    update-employee

--- update-employee.sql
+++ update-employee.sql
@@ -1,5 +1,5 @@
 UPDATE employees
 SET Title     = 'IT Manager',
-    ReportsTo = (SELECT EmployeeId FROM employees WHERE LastName = 'Andrew' AND FirstName = 'Adams')
+    ReportsTo = (SELECT EmployeeId FROM employees WHERE LastName = 'Adams' AND FirstName = 'Andrew')
 WHERE LastName = 'King'
   AND FirstName = 'Robert';
