c4960b0969ff5db2c9ba75edc8743e844f23598d
antaww <anta.pizzetta@gmail.com>
2022-12-26 18:39:24 +0100

    create-table

---
+++ create-table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE students
+(
+    StudentId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    FirstName VARCHAR(80) NOT NULL,
+    LastName  VARCHAR(80) NOT NULL,
+    Birthday  DATE        NOT NULL,
+    City      VARCHAR(50) NOT NULL
+);
