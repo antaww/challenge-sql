cb7766252aa3e57f81b3d0c2244b1d4aa324152a
antaww <anta.pizzetta@gmail.com>
2022-12-26 18:54:09 +0100

    insert-employee

--- insert-employee.sql
+++ insert-employee.sql
@@ -1,4 +1,4 @@
 INSERT INTO employees (LastName, FirstName, Title, ReportsTo, BirthDate, HireDate, Address, City, State, Country,
                        PostalCode, Phone, Fax, Email)
-VALUES ('Doe', 'John', 'IT Developer', 5, '1999-03-18', '2020-03-18', '142 rue Sainte-Catherine', 'Bordeaux', 'NA',
+VALUES ('Doe', 'John', 'IT Developer', 6, '1999-03-18', '2020-03-18', '142 rue Sainte-Catherine', 'Bordeaux', 'NA',
         'France', '33000', '+33 6 77 88 99 10', '+33 5 68 89 90 90', 'john.doe@gmail.com');
