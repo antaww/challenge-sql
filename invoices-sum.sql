c8ba491ac4585488a828a605be54768f921dd8ac
antaww <anta.pizzetta@gmail.com>
2023-01-11 14:04:53 +0100

    refactor(part3): Remove round from invoices-sum.sql

--- invoices-sum.sql
+++ invoices-sum.sql
@@ -1,3 +1,3 @@
-SELECT ROUND(SUM(Total),2) AS AllInvoicesTotalPrice
+SELECT SUM(Total) AS AllInvoicesTotalPrice
 FROM invoices
 WHERE CustomerId = (SELECT CustomerId FROM customers WHERE FirstName = 'Tim' AND LastName = 'Goyer');
