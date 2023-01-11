325920d87809f04ace4aa5c0894b0d6b32ca35dd
antaww <anta.pizzetta@gmail.com>
2023-01-11 14:02:38 +0100

    refactor(part3): Remove comments from customers-invoices-avg.sql

--- customers-invoices-avg.sql
+++ customers-invoices-avg.sql
@@ -1,15 +1,3 @@
--- You must carry out a statistical study on all the customers as well as their
--- invoices.
---
--- You must select, the FirstName and the LastName of each customer,
--- as well as the average total price of all their respective invoices with
--- the alias InvoicesAverage.
---
--- Take a look at the GROUP BY clause. You must GROUP BY an ID to be sure
--- that you haven't 2 same names.
---
--- You must order result by FirstName in ascending order.
-
 SELECT
     c.FirstName,
     c.LastName,
