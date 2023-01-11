9c1d41dba9a72eaa49cfb1c8d1eb9ea10bc5b936
antaww <anta.pizzetta@gmail.com>
2023-01-11 14:37:09 +0100

    refactor(part3): Upper LastName in total-invoices.sql

--- total-invoices.sql
+++ total-invoices.sql
@@ -1,4 +1,4 @@
-SELECT printf('%s %s', c.FirstName, c.LastName) AS FullName,
+SELECT printf('%s %s', c.FirstName, UPPER(c.LastName)) AS FullName,
 ROUND(SUM(i.Total),2) AS AllInvoices
 FROM customers c
 INNER JOIN invoices i ON c.CustomerId = i.CustomerId
