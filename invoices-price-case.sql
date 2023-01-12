2a0b1f4086621e8e61e7d5d3dc2d147761b1310d
antaww <anta.pizzetta@gmail.com>
2023-01-12 16:25:12 +0100

    fix(part4): Fix invoices-price-case.sql

--- invoices-price-case.sql
+++ invoices-price-case.sql
@@ -1,8 +1,8 @@
-SELECT i.InvoiceId, i.Total AS InvoiceTotalPrice,
+SELECT InvoiceId, Total AS InvoiceTotalPrice,
 CASE
-   WHEN i.Total < 5 THEN 'Price lower than 5$'
-   WHEN i.Total < 10 THEN 'Price lower than 10$'
-   ELSE 'Price greater than 10$'
-   END AS PriceCase
-FROM invoices i
+    WHEN Total < 5 THEN 'Price lower than 5$'
+    WHEN Total < 10 THEN 'Price lower than 10$'
+    ELSE 'Price greater than 10$'
+END AS 'CASE'
+FROM invoices
 LIMIT 100;
