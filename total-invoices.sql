07c3aa16bfcefbd30089ffd90f4220ff270d3aba
antaww <anta.pizzetta@gmail.com>
2023-01-12 16:17:46 +0100

    fix(part4): Fix invoices-price-case.sql

--- total-invoices.sql
+++ total-invoices.sql
@@ -1,7 +1,5 @@
-SELECT printf('%s %s', c.FirstName, UPPER(c.LastName)) AS FullName,
-ROUND(SUM(i.Total),2) AS AllInvoices
-FROM customers c
-INNER JOIN invoices i ON c.CustomerId = i.CustomerId
-GROUP BY c.CustomerId
+SELECT printf('%s %s', FirstName, UPPER(LastName)) AS FullName, SUM(Total) AS AllInvoices
+FROM customers                                                                             INNER JOIN invoices ON customers.CustomerId = invoices.CustomerId
+GROUP BY customers.CustomerId
 HAVING AllInvoices > 38
 ORDER BY FullName;
