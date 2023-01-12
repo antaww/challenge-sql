6dc058f910e5cbd61fe6cdd1be54b9e17e81e90b
antaww <anta.pizzetta@gmail.com>
2023-01-12 17:26:40 +0100

    fix(part4): Fix artist-albums-case.sql

--- artist-albums-case.sql
+++ artist-albums-case.sql
@@ -1,10 +1,10 @@
-SELECT a.Name as Name, COUNT(al.AlbumId) AS NbAlbums,
+SELECT ar.Name as Name, COUNT(al.AlbumId) AS NbAlbums,
 CASE
    WHEN COUNT(al.AlbumId) = 1 THEN 'Unproductive'
    WHEN COUNT(al.AlbumId) >= 10 THEN 'Very Productive'
    WHEN COUNT(al.AlbumId) > 1 THEN 'Productive'
 END AS IsProductive
-FROM artists a
-INNER JOIN albums al ON a.ArtistId = al.ArtistId
-GROUP BY a.Name
+FROM artists ar
+INNER JOIN albums al ON ar.ArtistId = al.ArtistId
+GROUP BY ar.ArtistId
 LIMIT 100;
