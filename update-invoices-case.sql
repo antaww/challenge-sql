8b396854900ed3fe1a2bb945af79e793c6cf5051
antaww <anta.pizzetta@gmail.com>
2023-01-12 16:37:13 +0100

    fix(part4): Fix xupdate-invoices-case.sql

--- update-invoices-case.sql
+++ update-invoices-case.sql
@@ -1,8 +1,7 @@
-SELECT *,
+UPDATE invoices
+SET Total =
 CASE
     WHEN BillingCountry = 'France' THEN Total + 3
     WHEN BillingCountry = 'USA' THEN Total + 5
     ELSE Total + 1
-END
-FROM invoices;
-
+END;
